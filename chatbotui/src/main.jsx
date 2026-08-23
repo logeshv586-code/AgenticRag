7d6547c00e6820b96e231fcaffab85fce421aeeb
logesh <logeshv586@gmail.com>
2026-08-24 02:34:07 +0530

    Make chat-first RAG factory the default customer experience

--- chatbotui/src/main.jsx
+++ chatbotui/src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import App from './App.jsx'
 import StandaloneChat from './StandaloneChat.jsx'
 import CustomerRagStudio from './CustomerRagStudio.jsx'
+import ChatRagStudio from './ChatRagStudio.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CustomerRagStudio />} />
+        <Route path="/" element={<ChatRagStudio />} />
+        <Route path="/starter" element={<CustomerRagStudio />} />
         <Route path="/advanced" element={<App />} />
         <Route path="/chat/:pipelineId" element={<StandaloneChat />} />
       </Routes>
