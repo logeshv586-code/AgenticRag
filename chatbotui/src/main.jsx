9ca149577f38abad064352e757867330113751cc
logesh <logeshv586@gmail.com>
2026-08-25 01:29:17 +0530

    Use premium Autopilot studio as default UI

--- chatbotui/src/main.jsx
+++ chatbotui/src/main.jsx
@@ -5,13 +5,13 @@ import './index.css'
 import App from './App.jsx'
 import StandaloneChat from './StandaloneChat.jsx'
 import CustomerRagStudio from './CustomerRagStudio.jsx'
-import ChatRagStudio from './ChatRagStudio.jsx'
+import OmniRagAutopilotStudio from './OmniRagAutopilotStudio.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ChatRagStudio />} />
+        <Route path="/" element={<OmniRagAutopilotStudio />} />
         <Route path="/starter" element={<CustomerRagStudio />} />
         <Route path="/advanced" element={<App />} />
         <Route path="/chat/:pipelineId" element={<StandaloneChat />} />
